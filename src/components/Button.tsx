7eb7620e9cc390af7a6e67d0e25d99529a21c4b3
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-17 22:05:12 +0900

    feat: 버튼 컴포넌트- 추후 스타일 수정 필요

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,25 @@
-const Button = () => {
-  return <div>Button</div>;
+import { CSSProperties, DetailedHTMLProps } from 'react';
+import styled from 'styled-components';
+
+type Props = DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  title?: string;
+  width?: string | number;
+  height?: string | number;
+  background?: string | number;
+  color?: string | number;
 };
 
+const Button = (props: Props) => {
+  return <ButtonComponent {...props}>{props.title}</ButtonComponent>;
+};
 export default Button;
+
+const ButtonComponent = styled.button<CSSProperties>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  background: ${({ background }) => background};
+  color: ${({ color }) => color};
+`;
