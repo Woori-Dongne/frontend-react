b58f976c89368d1e5b625cb4645a4739629139a1
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-18 20:25:28 +0900

    feat: 로그인 레이아웃

--- src/pages/Login/Login.style.ts
+++ src/pages/Login/Login.style.ts
@@ -1,3 +1,26 @@
 import { styled } from 'styled-components';
+import { mixins } from '../../styles/mixins';
+// padding: 348px 55px 72px 55px;
+export const Container = styled.div`
+  height: 81vh;
+  padding-top: 10vh;
+  gap: 20vh;
 
-export const Container = styled.div``;
+  ${mixins.columnFlexBox('center', 'center')}
+`;
+
+export const LogoContainer = styled.div`
+  ${mixins.columnFlexBox()}
+`;
+
+export const LogoIcon = styled.img`
+  width: 260px;
+`;
+
+export const LogoText = styled.img`
+  width: 130px;
+`;
+
+export const ButtonContainer = styled.div`
+  ${mixins.flexBox('center', '')}
+`;
